753880e4140daecc21bdc8a8f5d305d867ef8ccc
WinNode <117538854+Winnode@users.noreply.github.com>
2024-06-19 16:10:56 +0700

    Add files via upload

---
+++ Install.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Installing required packages...
+pip install aiohttp==3.9.5
+pip install aiofiles==23.2.1
+pip install fake-useragent==1.5.1
+pip install loguru==0.7.2
+pip install pyfiglet==0.8.post1
+echo Installation complete.
+pause
